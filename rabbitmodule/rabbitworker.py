004c23e6c72f673782af3800fc8a199f76145b49
AleksandrTan <rumych2013@gmail.com>
2021-05-19 11:11:40 +0300

    Core add - 2

--- rabbitmodule/rabbitworker.py
+++ rabbitmodule/rabbitworker.py
@@ -3,11 +3,28 @@ Server class RabbitWorker
 """
 import pika
 import config
-from logsource.logmodule import LogModule
+# from logsource.logmodule import LogModule
 
 
-class RabbitWorker(LogModule):
+class RabbitWorker:
 
     def __init__(self):
         super().__init__()
-        self.connect = pika.BlockingConnection(pika.ConnectionParameters(config.RABBIT_HOST))
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.RABBIT_HOST))
+        self.channel = self.connection.channel()
+
+    def receive(self):
+        self.channel.queue_declare(queue="target")
+        self.channel.basic_consume(on_message_callback=RabbitWorker.callback, queue='target', auto_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
+
+    @staticmethod
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+
+if __name__ == "__main__":
+    worker = RabbitWorker()
+    worker.receive()
+
