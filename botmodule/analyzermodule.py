25cc4608680e175ad67d64212eebc89fa7b9cfa1
AleksandrTan <rumych2013@gmail.com>
2021-05-21 15:45:36 +0300

    Get content page - 2

--- botmodule/analyzermodule.py
+++ botmodule/analyzermodule.py
@@ -63,46 +63,33 @@ class AnalyzerModule:
         return {"status": status, "link_list": self.links_list, "button_links": self.button_links,
                 "count_link_list": self.count_link_list, "type_res": "analyzer_module"}
 
-    def parse_other_page(self, link_list: str) -> dict:
+    def parse_other_page(self, link_list: dict) -> dict:
         """
         Inbound page analyzer
         :param link_list:
         :return: dict
         """
-        content = self.request.get_content(link_list)
-        if not content["status"]:
-            return content
+        for link in link_list:
+            print(link)
+            content = self.request.get_content(link)
+            if not content["status"]:
+                return content
 
-        status = True
-        soup = bs(content["message"], "html.parser")
-        # select all job links
-        parent_obj = soup.find(settings.TARGET_LIST["parent_tag"], class_=settings.TARGET_LIST["parent_class"])
-        if parent_obj:
-            target_links = parent_obj.find_all(settings.TARGET_LIST["child_tag"],
-                                               class_=settings.TARGET_LIST["child_class"])
-            if target_links:
-                for link in target_links:
-                    target = link.find(settings.TARGET_LIST["single_child"]["target_tag"],
-                                       class_=settings.TARGET_LIST["single_child"]["target_class"])
-                    if target:
-                        self.links_list.append(config.TARGET_HOST + target["href"])
-                        continue
-                    else:
-                        continue
-                self.count_link_list = len(self.links_list)
+            status = True
+            soup = bs(content["message"], "html.parser")
 
-        # get link to the first vacancy (button)
-        button_link_parent = soup.find(settings.TARGET_BUTTON["parent_tag"],
-                                       class_=settings.TARGET_BUTTON["parent_class"])
-        if button_link_parent:
-            button_link = button_link_parent.find(settings.TARGET_BUTTON["single_child"]["target_tag"],
-                                                  class_=settings.TARGET_BUTTON["single_child"]["target_class"])
+            # get link to the first vacancy (button)
+            button_link_parent = soup.find(settings.TARGET_BUTTON["parent_tag"],
+                                           class_=settings.TARGET_BUTTON["parent_class"])
+            if button_link_parent:
+                button_link = button_link_parent.find(settings.TARGET_BUTTON["single_child"]["target_tag"],
+                                                      class_=settings.TARGET_BUTTON["single_child"]["target_class"])
 
-            if button_link and button_link.text == settings.TARGET_BUTTON["single_child"]["target_text"]:
-                self.button_links.append(button_link["href"] + settings.TARGET_BUTTON["single_child"]["google_string"])
+                if button_link and button_link.text == settings.TARGET_BUTTON["single_child"]["target_text"]:
+                    self.button_links.append(button_link["href"] + settings.TARGET_BUTTON["single_child"]["google_string"])
 
-        if not self.links_list:
-            status = False
+            if not self.links_list:
+                status = False
 
         return {"status": status, "link_list": self.links_list, "button_links": self.button_links,
                 "count_link_list": self.count_link_list, "type_res": "analyzer_module"}
--- botmodule/botworker.py
+++ botmodule/botworker.py
@@ -22,6 +22,11 @@ class BotWorker(LogModule):
         main_content = self.main_page_worker()
         if main_content["status"]:
             print(main_content)
+            button_links = self.other_page_worker(main_content["link_list"])
+            if button_links["status"]:
+                print(button_links)
+            else:
+                pass
         else:
             # send a report to the server, write log file
             print(main_content)
@@ -33,9 +38,10 @@ class BotWorker(LogModule):
         """
         return self.analyzer_module.parse_main_page(self.link)
 
-    def other_page_worker(self):
+    def other_page_worker(self, link_list: dict) -> dict:
         """
         Parsing the other link on page, and get buttons for form to send data
-        :return:
+        :param link_list: dict
+        :return: dict
         """
-        pass
+        return self.analyzer_module.parse_other_page(link_list)
