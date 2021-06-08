34e7dff68c15888c51045e0603b27edca42041de
alext <rumych2013@gmail.com>
2021-06-08 20:36:32 +0300

    home - 1

--- botmodule/analyzermodule.py
+++ botmodule/analyzermodule.py
@@ -107,7 +107,7 @@ class AnalyzerModule:
         for link in self.links_list:
             # update proxy server settings if needed
             if request_counter == config.NUMBER_REQUESTS:
-                proxy = self.api_worker.update_proxy(self.proxy_worker.get_proxy_id())
+                proxy = self.api_worker.update_proxy()
                 if proxy:
                     print(proxy)
                     self.proxy_worker.set_proxy_data(proxy[1], proxy[0])
@@ -148,7 +148,7 @@ class AnalyzerModule:
         for button_link in self.button_links:
             # update proxy server settings if needed
             if request_counter == config.NUMBER_REQUESTS:
-                proxy = self.api_worker.update_proxy(self.proxy_worker.get_proxy_id())
+                proxy = self.api_worker.update_proxy()
                 if proxy:
                     print(proxy)
                     self.proxy_worker.set_proxy_data(proxy[1], proxy[0])
