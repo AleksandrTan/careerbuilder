aeeb34e8f2ad5a5f64d2f184a4d928b3a6493cea
AleksandrTan <rumych2013@gmail.com>
2021-06-15 18:00:07 +0300

    Dep - 1

--- botmodule/requestmodule.py
+++ botmodule/requestmodule.py
@@ -33,9 +33,10 @@ class RequestModule(LogModule):
         """
         count = 0
         session = HTMLSession()
-        session.proxies = self.proxy_worker.get_proxy_dict()
+        session.proxies = f = self.proxy_worker.get_proxy_dict()
         session.headers = settings.headers
         cookies = self.get_cookie()
+        print(link, f)
         while count < self.number_attempts:
             try:
                 response = session.get(link, timeout=(config.REQUEST_TIMEOUT, config.RESPONSE_TIMEOUT), cookies=cookies)
